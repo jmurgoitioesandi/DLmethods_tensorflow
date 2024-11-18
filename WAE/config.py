a02934c5d416fa91ed4be213a5f2a6bab98c9222
Javier Murgoitio Esandi <javiermurgoitioesandi@MacBookPro.attlocal.net>
2024-11-17 21:35:02 -0800

    right format

--- WAE/config.py
+++ WAE/config.py
@@ -1,33 +1,142 @@
 import argparse, textwrap
 
-formatter = lambda prog: argparse.HelpFormatter(prog,max_help_position=50)
+formatter = lambda prog: argparse.HelpFormatter(prog, max_help_position=50)
+
 
 def cla():
-    parser = argparse.ArgumentParser(description='list of arguments',formatter_class=formatter)
+    parser = argparse.ArgumentParser(
+        description="list of arguments", formatter_class=formatter
+    )
 
-    parser.add_argument('--dataname'   , type=str, required=True, help=textwrap.dedent('''Name of the file containing the dataset'''))
-    parser.add_argument('--slide_num'   , type=str, required=True, help=textwrap.dedent('''Slide identification number'''))
-    parser.add_argument('--n_train'   , type=int, default=4000, help=textwrap.dedent('''Number of training samples to use. Cannot be more than that available.'''))
-    parser.add_argument('--reg_param' , type=float, default=1e-7, help=textwrap.dedent('''Regularization parameter'''))
-    parser.add_argument('--n_epoch'   , type=int, default=1000, help=textwrap.dedent('''Maximum number of epochs'''))
-    parser.add_argument('--z_dim'     , type=int, default=1, help=textwrap.dedent('''Dimension of the latent variable, when using type1 C-GAN'''))
-    parser.add_argument('--lambda_param'     , type=float, default=1., help=textwrap.dedent('''Beta weight on the KL regularization of the loss function'''))
-    parser.add_argument('--batch_size', type=int, default=16, help=textwrap.dedent('''Batch size while training'''))
-    parser.add_argument('--learn_rate', type=float, default=1e-3, help=textwrap.dedent('''Initial Learning rate'''))
-    parser.add_argument('--lr_sched', type=bool, default=True, help=textwrap.dedent('''Learning rate schedule'''))	
-    parser.add_argument('--act_function', type=str, default='ReLU', help=textwrap.dedent('''Activation function name'''))
-    parser.add_argument('--denseblock_n', type=int, default=3, help=textwrap.dedent('''Number of blocks in denseblock'''))
+    parser.add_argument(
+        "--dataname",
+        type=str,
+        required=True,
+        help=textwrap.dedent("""Name of the file containing the dataset"""),
+    )
+    parser.add_argument(
+        "--slide_num",
+        type=str,
+        required=True,
+        help=textwrap.dedent("""Slide identification number"""),
+    )
+    parser.add_argument(
+        "--n_train",
+        type=int,
+        default=4000,
+        help=textwrap.dedent(
+            """Number of training samples to use. Cannot be more than that available."""
+        ),
+    )
+    parser.add_argument(
+        "--reg_param",
+        type=float,
+        default=1e-7,
+        help=textwrap.dedent("""Regularization parameter"""),
+    )
+    parser.add_argument(
+        "--n_epoch",
+        type=int,
+        default=1000,
+        help=textwrap.dedent("""Maximum number of epochs"""),
+    )
+    parser.add_argument(
+        "--z_dim",
+        type=int,
+        default=1,
+        help=textwrap.dedent(
+            """Dimension of the latent variable, when using type1 C-GAN"""
+        ),
+    )
+    parser.add_argument(
+        "--lambda_param",
+        type=float,
+        default=1.0,
+        help=textwrap.dedent(
+            """Beta weight on the KL regularization of the loss function"""
+        ),
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help=textwrap.dedent("""Batch size while training"""),
+    )
+    parser.add_argument(
+        "--learn_rate",
+        type=float,
+        default=1e-3,
+        help=textwrap.dedent("""Initial Learning rate"""),
+    )
+    parser.add_argument(
+        "--lr_sched",
+        type=bool,
+        default=True,
+        help=textwrap.dedent("""Learning rate schedule"""),
+    )
+    parser.add_argument(
+        "--act_function",
+        type=str,
+        default="ReLU",
+        help=textwrap.dedent("""Activation function name"""),
+    )
+    parser.add_argument(
+        "--denseblock_n",
+        type=int,
+        default=3,
+        help=textwrap.dedent("""Number of blocks in denseblock"""),
+    )
 
-    parser.add_argument('--seed_no'      , type=int, default=1008, help=textwrap.dedent('''Fix the random seed'''))
-    parser.add_argument('--savefig_freq' , type=int, default=100, help=textwrap.dedent('''Number of epochs after which a snapshot and plots are saved'''))
-    parser.add_argument('--save_suffix'  , type=str, default='', help=textwrap.dedent('''Suffix to directory where trained network/results are saved'''))
-    parser.add_argument('--sample_plots' , type=int, default=5, help=textwrap.dedent('''Number of validation samples used to generate plots'''))
-    
+    parser.add_argument(
+        "--seed_no",
+        type=int,
+        default=1008,
+        help=textwrap.dedent("""Fix the random seed"""),
+    )
+    parser.add_argument(
+        "--savefig_freq",
+        type=int,
+        default=100,
+        help=textwrap.dedent(
+            """Number of epochs after which a snapshot and plots are saved"""
+        ),
+    )
+    parser.add_argument(
+        "--save_suffix",
+        type=str,
+        default="",
+        help=textwrap.dedent(
+            """Suffix to directory where trained network/results are saved"""
+        ),
+    )
+    parser.add_argument(
+        "--sample_plots",
+        type=int,
+        default=5,
+        help=textwrap.dedent("""Number of validation samples used to generate plots"""),
+    )
 
-    parser.add_argument('--data_suffix'  , type=str, default='', help=textwrap.dedent('''Suffix to test results directory where results on test data are saved'''))
-    parser.add_argument('--main_dir'     , type=str, default='exps', help=textwrap.dedent('''Parent directory saving the various versions of trained networks'''))
-    parser.add_argument('--checkpoint_id', type=int, default=-1, help=textwrap.dedent('''The checkpoint index to load when testing'''))
-    
+    parser.add_argument(
+        "--data_suffix",
+        type=str,
+        default="",
+        help=textwrap.dedent(
+            """Suffix to test results directory where results on test data are saved"""
+        ),
+    )
+    parser.add_argument(
+        "--main_dir",
+        type=str,
+        default="exps",
+        help=textwrap.dedent(
+            """Parent directory saving the various versions of trained networks"""
+        ),
+    )
+    parser.add_argument(
+        "--checkpoint_id",
+        type=int,
+        default=-1,
+        help=textwrap.dedent("""The checkpoint index to load when testing"""),
+    )
+
     return parser.parse_args()
-
-
--- WAE/models.py
+++ WAE/models.py
@@ -9,193 +9,283 @@ from functools import partial
 from config import cla
 
 PARAMS = cla()
-Act_func      = PARAMS.act_function
-denseblock_n  = PARAMS.denseblock_n
+Act_func = PARAMS.act_function
+denseblock_n = PARAMS.denseblock_n
 
 np.random.seed(1008)
 
-Act_param  = 0.2
-use_bias   = True
+Act_param = 0.2
+use_bias = True
 
 
-if Act_func == 'ReLU':
-  activation_func = keras.layers.ReLU(negative_slope=Act_param)
-elif Act_func=='ELU':
-  activation_func = keras.activations.elu
-elif Act_func == 'tanh':  
-  activation_func = keras.layers.Activation('tanh')
-elif Act_func == 'sigmoid':  
-  activation_func = keras.layers.Activation('sigmoid')  
-elif Act_func == 'sin':
-  activation_func = tf.math.sin
+if Act_func == "ReLU":
+    activation_func = keras.layers.ReLU(negative_slope=Act_param)
+elif Act_func == "ELU":
+    activation_func = keras.activations.elu
+elif Act_func == "tanh":
+    activation_func = keras.layers.Activation("tanh")
+elif Act_func == "sigmoid":
+    activation_func = keras.layers.Activation("sigmoid")
+elif Act_func == "sin":
+    activation_func = tf.math.sin
 
-def DenseBlock(input_X,normalization=None,reg_param=1.0e-7, n=3, input_Z=None):
-  
-  N,H,W,Nx = input_X.shape
 
-  padding = tf.constant([[0,0],[1,1],[1,1], [0, 0]])
+def DenseBlock(input_X, normalization=None, reg_param=1.0e-7, n=3, input_Z=None):
 
-  X = input_X
+    N, H, W, Nx = input_X.shape
 
-  X_list = [X]
+    padding = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
 
-  for i in range(n):
-    if i == 0:
-      X = X_list[0]
-    else:
-      X = keras.layers.concatenate(X_list, axis=-1)
-    if normalization == "ln":
-      X = tf.keras.layers.LayerNormalization()(X)
-    elif normalization == "bn":
-      X = tf.keras.layers.BatchNormalization()(X)
-    X = activation_func(X)
-    X  = tf.pad(X,padding,"REFLECT")
-    X  = keras.layers.Conv2D(filters=Nx,
-                             kernel_size=3,
-                             strides=1,
-                             padding='valid',
-                             kernel_regularizer=keras.regularizers.l2(reg_param),
-                             use_bias=use_bias
-                             )(X)
-    X_list.append(X)
-                                                  
-  return X  
-
-def DownSample(input_X,k,downsample=True,activation=True,do_padding=True,reg_param=1.0e-7):
-
-  padding = tf.constant([[0,0],[1,1],[1,1], [0, 0]])
-
-  if do_padding:
-    X  = tf.pad(input_X,padding,"REFLECT")
-  else:
     X = input_X
-  X  = keras.layers.Conv2D(filters=k,
-                           kernel_size=3,
-                           strides=1,
-                           padding='valid',
-                           kernel_regularizer=keras.regularizers.l2(reg_param),
-                           use_bias=use_bias)(X)
-  if activation:
-    X = activation_func(X)                          
-  if downsample:
-    X = keras.layers.AveragePooling2D(pool_size=2,strides=2)(X)
-                                                
-  return X
+
+    X_list = [X]
+
+    for i in range(n):
+        if i == 0:
+            X = X_list[0]
+        else:
+            X = keras.layers.concatenate(X_list, axis=-1)
+        if normalization == "ln":
+            X = tf.keras.layers.LayerNormalization()(X)
+        elif normalization == "bn":
+            X = tf.keras.layers.BatchNormalization()(X)
+        X = activation_func(X)
+        X = tf.pad(X, padding, "REFLECT")
+        X = keras.layers.Conv2D(
+            filters=Nx,
+            kernel_size=3,
+            strides=1,
+            padding="valid",
+            kernel_regularizer=keras.regularizers.l2(reg_param),
+            use_bias=use_bias,
+        )(X)
+        X_list.append(X)
+
+    return X
 
 
-def UpSample(input_X,k,upsample=True,activation=True,do_padding=True,reg_param=1.0e-7):
+def DownSample(
+    input_X, k, downsample=True, activation=True, do_padding=True, reg_param=1.0e-7
+):
 
-  padding = tf.constant([[0,0],[1,1],[1,1], [0, 0]])
+    padding = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
+
+    if do_padding:
+        X = tf.pad(input_X, padding, "REFLECT")
+    else:
+        X = input_X
+    X = keras.layers.Conv2D(
+        filters=k,
+        kernel_size=3,
+        strides=1,
+        padding="valid",
+        kernel_regularizer=keras.regularizers.l2(reg_param),
+        use_bias=use_bias,
+    )(X)
+    if activation:
+        X = activation_func(X)
+    if downsample:
+        X = keras.layers.AveragePooling2D(pool_size=2, strides=2)(X)
+
+    return X
 
 
-  X = input_X  
-  if do_padding:
-    X  = tf.pad(X,padding,"REFLECT")
-  X  = keras.layers.Conv2D(filters=k,
-                          kernel_size=3,
-                          strides=1,
-                          padding='valid',
-                          kernel_regularizer=keras.regularizers.l2(reg_param),
-                          use_bias=use_bias)(X)
-  if activation:
-    X = activation_func(X)
+def UpSample(
+    input_X, k, upsample=True, activation=True, do_padding=True, reg_param=1.0e-7
+):
 
-  if upsample:
-    X = keras.layers.UpSampling2D(size=2)(X)
-                                                
-  return X  
+    padding = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
 
-def encoder(Input_W=32,Input_H=32,Input_C=4,latent_dim=100, k0=36,reg_param=1.0e-7, norm_type = 'ln',denseblock_size=denseblock_n):
+    X = input_X
+    if do_padding:
+        X = tf.pad(X, padding, "REFLECT")
+    X = keras.layers.Conv2D(
+        filters=k,
+        kernel_size=3,
+        strides=1,
+        padding="valid",
+        kernel_regularizer=keras.regularizers.l2(reg_param),
+        use_bias=use_bias,
+    )(X)
+    if activation:
+        X = activation_func(X)
 
-  # We are asuming that X and Y have the same dimensions and same number of chanels
-  input_XY = keras.Input(shape=(Input_W,Input_H,Input_C))
+    if upsample:
+        X = keras.layers.UpSampling2D(size=2)(X)
 
-  # Downsampling + ResBlock
-  X0  = DownSample(input_X=input_XY,downsample=False,k=k0,do_padding=True,reg_param=reg_param)
-  X0  = DenseBlock(input_X=X0, normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+    return X
 
-  X1  = DownSample(input_X=X0,k=k0,do_padding=True,reg_param=reg_param)
-  X1  = DenseBlock(input_X=X1, normalization=norm_type,reg_param=reg_param,n=denseblock_size)
-  
-  X2  = DownSample(input_X=X1,k=2*k0,do_padding=True,reg_param=reg_param)
-  X2  = DenseBlock(input_X=X2,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
 
-  X3  = DownSample(input_X=X2,k=4*k0,do_padding=True,reg_param=reg_param)
-  X3  = DenseBlock(input_X=X3,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+def encoder(
+    Input_W=32,
+    Input_H=32,
+    Input_C=4,
+    latent_dim=100,
+    k0=36,
+    reg_param=1.0e-7,
+    norm_type="ln",
+    denseblock_size=denseblock_n,
+):
 
-  X4  = DownSample(input_X=X3,k=8*k0,do_padding=True,reg_param=reg_param)
-  X4  = DenseBlock(input_X=X4,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+    # We are asuming that X and Y have the same dimensions and same number of chanels
+    input_XY = keras.Input(shape=(Input_W, Input_H, Input_C))
 
-  X5  = keras.layers.Flatten()(X4)
+    # Downsampling + ResBlock
+    X0 = DownSample(
+        input_X=input_XY, downsample=False, k=k0, do_padding=True, reg_param=reg_param
+    )
+    X0 = DenseBlock(
+        input_X=X0, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
 
-  X5  = keras.layers.Dense(units=latent_dim*3,
-                           activation=activation_func,
-                           kernel_regularizer=keras.regularizers.l2(reg_param),
-                           use_bias=use_bias)(X5)
+    X1 = DownSample(input_X=X0, k=k0, do_padding=True, reg_param=reg_param)
+    X1 = DenseBlock(
+        input_X=X1, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
 
-  X6  = keras.layers.Dense(units=latent_dim,
-                           activation=activation_func,
-                           kernel_regularizer=keras.regularizers.l2(reg_param),
-                           use_bias=use_bias)(X5)
-  X7  = keras.layers.BatchNormalization()(X6)                         
-  X8  = keras.layers.Dense(units=latent_dim,
-                           activation=None,
-                           kernel_regularizer=keras.regularizers.l2(reg_param),
-                           use_bias=use_bias)(X7)                   
-  
-  model = keras.Model(inputs=input_XY, outputs=X8)
+    X2 = DownSample(input_X=X1, k=2 * k0, do_padding=True, reg_param=reg_param)
+    X2 = DenseBlock(
+        input_X=X2, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
 
-  return model
+    X3 = DownSample(input_X=X2, k=4 * k0, do_padding=True, reg_param=reg_param)
+    X3 = DenseBlock(
+        input_X=X3, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
 
-def decoder(latent_dim=100, cnn_init_dim = (2,2,512), output_channels = 4, reg_param=1.0e-7, norm_type = 'bn', denseblock_size=denseblock_n):
+    X4 = DownSample(input_X=X3, k=8 * k0, do_padding=True, reg_param=reg_param)
+    X4 = DenseBlock(
+        input_X=X4, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
 
-  input_z = keras.Input(shape=(latent_dim))
+    X5 = keras.layers.Flatten()(X4)
 
-  units_init = cnn_init_dim[0]*cnn_init_dim[1]*cnn_init_dim[2]
-  
-  X1 =  tf.keras.layers.Dense(units=units_init)(input_z)
+    X5 = keras.layers.Dense(
+        units=latent_dim * 3,
+        activation=activation_func,
+        kernel_regularizer=keras.regularizers.l2(reg_param),
+        use_bias=use_bias,
+    )(X5)
 
-  X2 = tf.keras.layers.Reshape(target_shape=cnn_init_dim)(X1)
+    X6 = keras.layers.Dense(
+        units=latent_dim,
+        activation=activation_func,
+        kernel_regularizer=keras.regularizers.l2(reg_param),
+        use_bias=use_bias,
+    )(X5)
+    X7 = keras.layers.BatchNormalization()(X6)
+    X8 = keras.layers.Dense(
+        units=latent_dim,
+        activation=None,
+        kernel_regularizer=keras.regularizers.l2(reg_param),
+        use_bias=use_bias,
+    )(X7)
 
-  X3 = UpSample(input_X=X2,k=int(cnn_init_dim[2]/2),upsample=True,activation=True,do_padding=True,reg_param=reg_param)
-  X3 = DenseBlock(input_X=X3,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+    model = keras.Model(inputs=input_XY, outputs=X8)
 
-  X4 = UpSample(input_X=X3,k=int(cnn_init_dim[2]/4),upsample=True,activation=True,do_padding=True,reg_param=reg_param)
-  X4 = DenseBlock(input_X=X4,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+    return model
 
-  X5 = UpSample(input_X=X4,k=int(cnn_init_dim[2]/8),upsample=True,activation=True,do_padding=True,reg_param=reg_param)
-  X5 = DenseBlock(input_X=X5,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
 
-  X6 = UpSample(input_X=X5,k=int(cnn_init_dim[2]/16),upsample=True,activation=True,do_padding=True,reg_param=reg_param)
-  X6 = DenseBlock(input_X=X6,normalization=norm_type,reg_param=reg_param,n=denseblock_size)
+def decoder(
+    latent_dim=100,
+    cnn_init_dim=(2, 2, 512),
+    output_channels=4,
+    reg_param=1.0e-7,
+    norm_type="bn",
+    denseblock_size=denseblock_n,
+):
 
-  X7 = UpSample(input_X=X6,k=output_channels,upsample=False,activation=False,do_padding=True,reg_param=reg_param)
+    input_z = keras.Input(shape=(latent_dim))
 
-  X7 = tf.keras.activations.sigmoid(X7)
+    units_init = cnn_init_dim[0] * cnn_init_dim[1] * cnn_init_dim[2]
 
-  model = tf.keras.Model(inputs=input_z, outputs=X7)
-  
-  return model
-    
+    X1 = tf.keras.layers.Dense(units=units_init)(input_z)
+
+    X2 = tf.keras.layers.Reshape(target_shape=cnn_init_dim)(X1)
+
+    X3 = UpSample(
+        input_X=X2,
+        k=int(cnn_init_dim[2] / 2),
+        upsample=True,
+        activation=True,
+        do_padding=True,
+        reg_param=reg_param,
+    )
+    X3 = DenseBlock(
+        input_X=X3, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
+
+    X4 = UpSample(
+        input_X=X3,
+        k=int(cnn_init_dim[2] / 4),
+        upsample=True,
+        activation=True,
+        do_padding=True,
+        reg_param=reg_param,
+    )
+    X4 = DenseBlock(
+        input_X=X4, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
+
+    X5 = UpSample(
+        input_X=X4,
+        k=int(cnn_init_dim[2] / 8),
+        upsample=True,
+        activation=True,
+        do_padding=True,
+        reg_param=reg_param,
+    )
+    X5 = DenseBlock(
+        input_X=X5, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
+
+    X6 = UpSample(
+        input_X=X5,
+        k=int(cnn_init_dim[2] / 16),
+        upsample=True,
+        activation=True,
+        do_padding=True,
+        reg_param=reg_param,
+    )
+    X6 = DenseBlock(
+        input_X=X6, normalization=norm_type, reg_param=reg_param, n=denseblock_size
+    )
+
+    X7 = UpSample(
+        input_X=X6,
+        k=output_channels,
+        upsample=False,
+        activation=False,
+        do_padding=True,
+        reg_param=reg_param,
+    )
+
+    X7 = tf.keras.activations.sigmoid(X7)
+
+    model = tf.keras.Model(inputs=input_z, outputs=X7)
+
+    return model
+
+
 class WAE(tf.keras.Model):
-  """Convolutional wasserstein autoencoder."""
+    """Convolutional wasserstein autoencoder."""
 
-  def __init__(self, latent_dim):
-    super(WAE, self).__init__()
-    self.latent_dim = latent_dim
-    self.encoder = encoder(latent_dim=latent_dim)
-    self.decoder = decoder(latent_dim=latent_dim)
+    def __init__(self, latent_dim):
+        super(WAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = encoder(latent_dim=latent_dim)
+        self.decoder = decoder(latent_dim=latent_dim)
 
-  @tf.function
-  def sample(self, n_size = 10, z=None):
-    if z is None:
-      z = tf.random.normal(shape=(n_size, self.latent_dim))
-    return self.decode(z)
+    @tf.function
+    def sample(self, n_size=10, z=None):
+        if z is None:
+            z = tf.random.normal(shape=(n_size, self.latent_dim))
+        return self.decode(z)
 
-  def encode(self, x):
-    z = self.encoder(x)
-    return z
+    def encode(self, x):
+        z = self.encoder(x)
+        return z
 
-  def decode(self, z):
-    reconstruction = self.decoder(z)
-    return reconstruction
+    def decode(self, z):
+        reconstruction = self.decoder(z)
+        return reconstruction
